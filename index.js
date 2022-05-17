837054bb163bd587c93ef4735a7b01d76d1166c9
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-05-17 15:07:34 +0530

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+// import {name} from './second'
+const disp = require('./second');
+const color = require('colors');
+console.log("red".red);
+console.log("blue".blue);
+console.log("green".green);
+console.log("these are the details",disp.details)
+// console.log()
---
+++ second.js
@@ -0,0 +1,10 @@
+const details = {
+    name:"keshav",
+    favnum : 34,
+    developer : true
+}
+module.exports = second;
+
+function name(){
+    console.log('hello')
+}
