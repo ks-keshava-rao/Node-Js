6b4ff67d428e57048ea0b04d41c1e6497882ca9d
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-05-23 19:06:44 +0530

    Update web.js

--- Express/routes/web.js
+++ Express/routes/web.js
@@ -2,6 +2,6 @@ import express from 'express'
 const router = express.Router();
 
 import controllerclass from '../controllers/controller.js'
-router.get('/getsessioninfo',controllerclass.get_session_info)
+router.get('/info',controllerclass.get_session_info)
 
 export default router;
