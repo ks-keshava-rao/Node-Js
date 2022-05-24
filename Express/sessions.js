f5c4f5797d68a01ee36ba85bcdb23cd98693ac5d
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-05-24 13:19:30 +0530

    session file updated

--- Express/sessions.js
+++ Express/sessions.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import router  from './routes/web.js'
+import session from 'express-session'
 const app = express();
 
 const Port = process.env.PORT || '3000';
 
+//Session
+app.use(session({
+    name : "first session",
+    secret : 'secretkey',
+    resave : false,
+    saveUninitialized : true,
+    cookie:{maxAge:200000}
+}))
 //load routes 
 app.use('/',router);
 app.listen(Port,()=>{
     console.log(`listening at http://localhost:${Port}`);
     console.log("server is running succesfully");
 });
+//default expriry age
