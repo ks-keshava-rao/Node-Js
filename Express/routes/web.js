f7bfb55018bf7d4f260337ad169156da9b13df58
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-05-24 13:19:19 +0530

    route handler

--- Express/routes/web.js
+++ Express/routes/web.js
@@ -2,6 +2,9 @@ import express from 'express'
 const router = express.Router();
 
 import controllerclass from '../controllers/controller.js'
-router.get('/info',controllerclass.get_session_info)
-
+router.get('/info',controllerclass.get_session_info);
+router.get('/delete',controllerclass.delete_session);
+router.get('/regnsession',controllerclass.regn_session);
+router.get('/sesscount',controllerclass.session_counter);
+router.get('/getdata',controllerclass.get_session_data);
 export default router;
